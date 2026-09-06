52826174bd4d33e139c4be8a01e3ccc6d17c2cbb
IslamMetwally <islammetwallyhac@gmail.com>
2026-09-06 14:55:31 +0300

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -11,5 +11,5 @@
 // ============================================================
 window.HAWKS_CONFIG = {
   supabaseUrl: 'https://armxrkxkuliahdtwjxod.supabase.co',
-  supabaseAnonKey: 'sb_publishable_vDT80qwpnSFID9-EHFv5WQ_bi7Xdwyh'
+  supabaseAnonKey: 'sb_secret_BqI2wcuW8t2kCU-aErDjYA_tpEPDONc'
 };
